726457e0650995f23e04a9de8fb01e1439fabd05
Adewale <ajayiadewale@yahoo.com>
2024-04-30 16:41:51 +0000

    todays

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
+
 const Dashboard = () => {
+    const [recentProducts, setRecentProducts] = useState([]);
+    const [categories, setCategories] = useState([]);
+    const [recentOrders, setRecentOrders] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    },[]);
+
+    const fetchData = async () => {
+        try{
+            const productsResponse = await fetch('https://fakestoreapi.com/products');
+            const productsData = await productsResponse
+            .then(res => res.json())
+            .then(json => console.log(json))
+            setRecentProducts(productsData.slice(0, 5));
+            
+            const categoriesResponse = await fetch('https://fakestoreapi.com/categories');
+            const categoriesData = await categoriesResponse
+            .then(res => res.json())
+            .then(json => console.log(json))
+            const categoriesWithCounts = categoriesData.map(category => ({
+                name: category,
+                productCount: productsData.filter(product => product.category === category.length)
+            }));
+            setCategories(categoriesWithCounts);
+
+            const ordersResponse = await fetch('https://fakestoreapi.com/orders');
+            const ordersData = await ordersResponse
+            .then(res => res.json())
+            .then(json => console.log(json))
+            setRecentOrders(ordersData.slice(0, 5));
+        } catch (error) {
+            console.error("Data fetching error:", error);
+        }
+    };
+
+    const renderTable = (data, columns) => (
+        <div className="table-container">
+            <table>
+                <thead>
+                    <tr>
+                        {columns.map((column, index) => (
+                            <th key={index}>{column}</th>
+                        ))}
+                    </tr>
+                </thead>
+                <tbody>
+                    {data.map((item, index) => (
+                        <tr key={index}>
+                            {columns.map((column, index) => (
+                                <td key={index}>{item[column.toLowerCase()]}</td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+
   return (
-    <div className="dashboard-container">
-      
-      </div>
-  );
+    <div className="dash-container">
+        <div className="section">
+            <h2 className="category-title">Recent Products</h2>
+            {renderTable(recentProducts, ['Title', 'Price', 'Description', 'Category'])}
+        </div>
+
+        <div className="section">
+            <h2 className="category-title">Categories</h2>
+            {renderTable(categories, ['Title', 'Price', 'Category', 'Description'])}
+        </div>
+
+        <div className="section">
+            <h2 className="category-title">Recent Orders</h2>
+            {renderTable(recentOrders, ['Title', 'Orders', 'Total', 'Status'])}
+        </div>
+    </div>
+  )
 }
 
 export default Dashboard;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,45 +1,66 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { login } from './authActions';
-import axios from 'axios';
+import { useForm } from "react-hook-form"
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../store/actions/authActions";
+import { useNavigate } from "react-router-dom";
 
-const LoginPage = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const dispatch = useDispatch();
-  const error = useSelector(state => state.auth.error);
+const Login = ({ handleLogin }) => {
+  const navigate = useNavigate()
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const dispatch = useDispatch();
 
+    const onSubmit = async (data) => {
+        console.log(data)
+        try {
+            const response = await fetch('https://fakestoreapi.com/auth/login', {
+                method: 'POST',
+                headers: {
+                  "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                username: "mor_2314",
+                password: "83r5^_"
+              }),
+            });   
+            
+            if (!response.ok) {
+                const errorResponse = await response.json();
+              throw new Error(errorResponse || 'Authentication failed');
+            }
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value);
-  }
+            const userData = await response.json();
+            
+            // Dispatch action storing user data
+            dispatch(loginSuccess(userData));
+            
+            // calling the handleLogin function
+            // handleLogin();
+            navigate('/dashboard');
+          } catch (error) {
+            console.error('Login error:', error.message);
+          }
+    };
   
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
-  }
-  
-  const handleSubmit = async (event) => {
-    event.preventDefault();
-    dispatch(login(username, password));
-  }
-
-    
-  return (
-    <div className='container'>
-      <h2 style={{textAlign:"center", fontSize:" 40px"}}>Login Page</h2>
-      <form onSubmit={handleSubmit}>
-        <div className='form'>
-          <label className='label'>Username:</label>
-          <input type='text' value={username} onChange={handleUsernameChange} />
-        </div>
-        <div className='form'>
-          <label className='label'>Password:</label>
-          <input type='password' value={password} onChange={handlePasswordChange} />
-        </div>
-        <button type='text'>Login</button>
-      </form>
-    </div>
-  );
+    return (
+        <>
+            <div className="container">
+                <h2 style={{textAlign:"center", fontSize:" 40px"}}>Login Page</h2>  
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="form">
+                        <label className='label'>Username:</label>
+                        <input type="text" {...register("username", { required: true })} />
+                        {errors.username && <span style={{color:"red"}}>Username is required</span>}
+                    </div>
+                    <div className="form">
+                        <label className='label'>Password:</label>
+                        <input type="password" {...register("password", { required: true })} />
+                        {errors.password && <span style={{color:"red"}}>Password is required</span>}
+                    </div>
+                     <button type='submit'>Login</button>
+                </form>      
+            </div>
+        </>
+      
+    )
 }
 
-export default LoginPage;
+export default Login
